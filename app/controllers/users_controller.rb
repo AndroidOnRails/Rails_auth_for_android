cd0dee98826337711ef1744d76544439ec508f1f
Jayasainath <jayasainaths@gmail.com>
2012-12-28 10:16:11 +0530

    Initial commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+
+	skip_before_filter :verify_authenticity_token
+
+	def list_users
+		@users = User.all
+		respond_to do |format|
+			format.html
+			format.json {render json: @users}
+		end
+	end
+
+	def create
+		@user = User.new(params[:user])
+		if @user.save
+			respond_to do |format|
+				# format.html
+				format.json {render json: @user}
+			end
+		else
+			respond_to do |format|
+				format.json {render json: @user}
+			end
+		end
+	end
+
+end
